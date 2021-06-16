468947d88d5c67c7b3bfb71bb596679344ef0af2
Shibumishra <sibumishra487@gmail.com>
2021-06-16 15:39:44 +0530

    read more
    Signed-off-by: Shibumishra <sibumishra487@gmail.com>

--- src/Filter.js
+++ src/Filter.js
@@ -5,7 +5,7 @@ import moment from "moment";
 const Filter = () => {
   const [data, setdata] = useState([]);
   const [product, setProduct] = useState([]);
-
+  const [readMore, setReadMore] = useState(false);
 
   useEffect(() => {
     console.log("Component Did Mount API");
@@ -14,34 +14,35 @@ const Filter = () => {
     });
   }, []);
 
-
   useEffect(() => {
     console.log("Component Did Mount API");
-    axios.get("https://60a004427ee1430017bf1440.mockapi.io/blog").then((res) => {
-      setProduct(res.data);
-    });
+    axios
+      .get("https://60a004427ee1430017bf1440.mockapi.io/blog")
+      .then((res) => {
+        setProduct(res.data);
+      });
   }, []);
 
- const filterItem = (button) => {
-   if(button === "All"){
-     setProduct()
-   }
+  const filterItem = (button) => {
+    if (button === "All") {
+      setProduct();
+    }
 
-   const filterData =  product.filter((currElem) => {
-     return currElem.name === button;
-   })
-   setProduct(filterData)
- }
+    const filterData = product.filter((currElem) => {
+      return currElem.name === button;
+    });
+    setProduct(filterData);
+  };
 
   return (
     <div>
       <div className="background">
-      <h3 className="latest_posts">Latest Posts</h3>
-      <div className="chatry">
-        <img
-          style={{ height: "3%", width: "3%" }}
-          alt="Filter Icon"
-          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAMAAABHPGVmAAAAbFBMVEUAAAA1gME1gME1f8A1gME1gMExd7Q0f8AxeLYzfb00f
+        <h3 className="latest_posts">Latest Posts</h3>
+        <div className="chatry">
+          <img
+            style={{ height: "3%", width: "3%" }}
+            alt="Filter Icon"
+            src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAMAAABHPGVmAAAAbFBMVEUAAAA1gME1gME1f8A1gME1gMExd7Q0f8AxeLYzfb00f
           r80f8Aze7oze7o1gMA0fr4zfLwzfb0zfb0yerg0fb00f8AzfLszfLwzfr40f780f8Azfb00f8A0f8AzfLwzfr81gME0fr4zfLs1gMEkRxAZAAAAI3RSTlMAHOP9f/
           oT1RAO281OROfhFaSUSkCwnpmOcTfFim9YB/AmGRclE4kAAAF9SURBVGje7dfrboJAEAXgA7jLXRAExRu2vP87No1NJjYpexm3SZv5/
           vgD9QB7mCwQQggh/rI2YtCwUpUxw7mGhTZdWMoWZlHMC4kjmL0Xv3Al8zHhZCQTbOid159/
@@ -49,56 +50,76 @@ const Filter = () => {
           JOw8M+NafQ4TKCl7pxeED28FRtrctbw9uk1u4YHdhW8Jd39LislXcCRzZalEt1OVj0YF76MQPTyVjkQYOtprlvmO4cfbysoOnOc6UiG6Y7Q36kIn8v7xWvkh1+
           mPvqmIPBtIExb0385z5/ups3MM+2b3ixi3nXy7eREAmREAmREAmRkPWQsw4Xkiz05hMqJEkoI0wIZTQVEC7k8VH0CGDztB5pDRKkXfTWHjJkOMGAX+
           Fdi0CowqMGCROiDpQRKkR1GcLZ0PtbODf1OUquOULKpzQ+VwitjTIIIYQQ/9wHYyfu5wm8dM0AAAAASUVORK5CYII="
-        />
-        <h4 className="filter_category">Filter by Category</h4>
-      </div>
-      <div className="filter_contar">
-        <button className="all" onClick={() => setProduct(product)}>All</button>
-        {data.length &&
-          data.map(({ title, id, }) => 
-          <div className="filted_data" onClick={() => filterItem(title)} key={id}>{title}</div>)}
-      </div>
+          />
+          <h4 className="filter_category">Filter by Category</h4>
+        </div>
+        <div className="filter_contar">
+          <button className="all" onClick={() => setProduct(product)}>
+            All
+          </button>
+          {data.length &&
+            data.map(({ title, id }) => (
+              <div
+                className="filted_data"
+                onClick={() => filterItem(title)}
+                key={id}
+              >
+                {title}
+              </div>
+            ))}
+        </div>
       </div>
 
-{/* //card -- OPEN*/}
-<div className="bg">
-      <div className="box">
-        {product.length &&
-          product.map(
-            ({
-              authorname,
-              description,
-              id,
-              posted_on,
-              slug,
-              small_image,
-              title,
-              name
-            }) => (
-              <div>
+      {/* //card -- OPEN*/}
+      <div className="bg">
+        <div className="box">
+          {product.length &&
+            product.map(
+              ({
+                authorname,
+                description,
+                id,
+                posted_on,
+                slug,
+                small_image,
+                title,
+                name,
+              }) => (
                 <div>
-                  <div key={id} className="card">
-                    <a href="https://www.edyoda.com/stories">
-                      <img
-                        class="image-dimensions"
-                        src={small_image}
-                        alt="apps"
-                      />
-                      <p className="title1">{title}</p>
-                      <span className="authorname"> {authorname} <span style={{color: "black"}}>|</span> <span className="time_day_month">{moment.utc(posted_on).format("DD MMM YYYY") }</span>
-                      </span>
-                      <p class="description">{description}</p>
-                    </a>
+                  <div>
+                    <div key={id} className="card">
+                      <a href="https://www.edyoda.com/stories">
+                        <img
+                          class="image-dimensions"
+                          src={small_image}
+                          alt="apps"
+                        />
+                        <p className="title1">{title}</p>
+                        <span className="authorname">
+                          {" "}
+                          {authorname} <span style={{ color: "black" }}>|</span>{" "}
+                          <span className="time_day_month">
+                            {moment.utc(posted_on).format("DD MMM YYYY")}
+                          </span>
+                        </span>
+                      </a>
+                        <p class="description">
+                          {readMore
+                            ? description
+                            : `${description.substring(0, 80)}...`}
+                          <button className="show-less-btn" onClick={() => setReadMore(!readMore)}>
+                            {readMore ? "show less" : "reas more"}
+                          </button>
+                        </p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            )
-          )}
+              )
+            )}
+        </div>
       </div>
-    </div>
 
-{/* //card -- END*/}
-
+      {/* //card -- END*/}
     </div>
   );
 };
